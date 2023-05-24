eb3798b11397b784ac307f847df4fcbefa6022b9
webdragon0125 <jakus.superdev@gmail.com>
2023-05-23 17:09:34 -0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import { Suspense } from 'react';
+import { Route, Routes, BrowserRouter as Router, Navigate } from 'react-router-dom';
+
+import { PUBLIC_ROUTES } from './config/routes';
+
+import { AppThemeProvider } from './Provider';
+import { Layout } from './layouts/layout';
+
+import { Home } from './pages/home';
+
+function App() {
+  return (
+    <Suspense fallback={<>Loading</>}>
+      <Router>
+        <AppThemeProvider>
+          <Layout>
+            <Routes>
+              <Route path={PUBLIC_ROUTES.default} element={<Home />} />
+            </Routes>
+          </Layout>
+        </AppThemeProvider>
+      </Router>
+    </Suspense>
+  );
+}
+
+export default App;
---
+++ src/pages/home.tsx
@@ -0,0 +1,11 @@
+import { TabPanel } from 'src/components/TabPanel';
+import { useStore } from 'src/context/StoreContext';
+
+export const Home = () => {
+  const { page } = useStore();
+  return (
+    <>
+      Home
+    </>
+  );
+};
